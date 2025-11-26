153a17c24461881777553f03a23d3a9164b6417d
AngelMaroto <angel.margar@educa.jcyl.es>
2025-11-26 10:20:45 +0100

    spring boot

---
+++ java-spring-boot-user-rest-api/src/main/java/it/juan/user/dao/UserDAO.java
@@ -0,0 +1,22 @@
+package it.juan.user.dao;
+
+import java.util.List;
+
+import it.juan.user.entity.User;
+
+
+public interface UserDAO {
+
+    public List<User> findAll();
+
+    public User findById(int id);
+
+    public User findByEmail(String email);
+
+    public void save(User user);
+
+    public void deleteById(int id);
+
+    public void deleteByEmail(String email);
+}
+
